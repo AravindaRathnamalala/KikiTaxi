f1adb0166759e861a7ed9fa749bc06e41b02123c
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2024-02-06 15:40:40 +0800

    Divide packages based on given criteria

--- kiki-lib/src/main/java/com/araa/kiki_lib/Constants.kt
+++ kiki-lib/src/main/java/com/araa/kiki_lib/Constants.kt
@@ -6,5 +6,6 @@ class Constants {
         const val BASE_DELIVERY_COST = 100
         const val WEIGHT_CONST = 10
         const val DISTANCE_CONST = 5
+        const val MAX_CAPACITY = 200
     }
 }
